d15834f3852c17e0ea954770884c26fdf6b49dec
Alex <ajoshua14@icloud.com>
2023-08-18 20:21:46 -0700

    Highlight outer div on input focus

--- src/components/inputBar/inputBar.tsx
+++ src/components/inputBar/inputBar.tsx
@@ -2,15 +2,23 @@
 
 'use client'
 
+import { useRef } from "react"
+import { RiVoiceprintLine } from 'react-icons/ri'
+import styles from './inputBar.module.css'
+
 export const InputBar = () => {
+  const msgRef = useRef<HTMLInputElement>(null);
+
   return (
     <div className="fixed bottom-0 w-full flex items-center h-16 px-2 pb-3 bg-tertiary glassmorphism">
-      <div className="flex-1 px-2">
-        <input type="text"
-          className="w-full px-3 py-1 rounded-full
-                    bg-input glassmorphism focus:outline-none focus:ring-2 ring-teal-600
-                    text-primary"
+      <div className={`flex-1 flex items-center px-3 py-1 rounded-full bg-input focus:ring-2 ring-teal-600 ${styles.inputWrapper}`}>
+        <input type="text" ref={msgRef}
+          className={`w-full 
+                    bg-input glassmorphism focus:outline-none
+                    ${styles.input}`}
+          spellCheck="true"
         />
+        <RiVoiceprintLine />
       </div>
       <div className="px-2">
         <button>Search</button>
