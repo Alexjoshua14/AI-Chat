aadd6fb66d9ab793dbee246a9fbc9c266b20e893
Alex <ajoshua14@icloud.com>
2023-08-21 12:34:06 -0700

    Update msg object fields

--- src/components/message.tsx
+++ src/components/message.tsx
@@ -1,26 +1,14 @@
 /* Alex Joshua (c) 2023 */
 
-type User = {
-  id: string
-  username: string
-  color: string
-}
+import { Message } from '@/types'
 
-type Message = {
-  id: string
-  userId: string
-  user: User
-  content: string
-  timestamp: Date
-}
-
-export const Message = ({ msg, className, ...props }: { msg: Message, className?: String }) => {
+export const MessageCard = ({ msg, className, ...props }: { msg: Message, className?: String }) => {
 
   return (
     <div className={`grid grid-cols-msg grid-rows-msg gap-2 center ${className}`} {...props}>
-      <div className={`w-5 h-5 rounded-full bg-${msg.user.color}-500 glassmorphism`} />
+      <div className={`w-5 h-5 rounded-full bg-${msg.author.color}-500 glassmorphism`} />
       <h2 className="text-secondary font-extralight text-xs">
-        {msg.user.username}
+        {msg.author.username}
       </h2>
 
       <div className="col-start-2">{msg.content}</div>
