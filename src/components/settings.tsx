f827805d214ee332c3d2a449075f642c7819cbc5
Alex <ajoshua14@icloud.com>
2023-08-23 12:55:56 -0700

    Update z position and absolute positioning

--- src/components/settings.tsx
+++ src/components/settings.tsx
@@ -4,7 +4,7 @@ import { PiDotsThreeBold } from "react-icons/pi";
 
 export const Settings = () => {
   return (
-    <div className="fixed flex top-4 right-4 h-10 w-12 bg-tertiary rounded-md glassmorphism">
+    <div className="fixed flex top-4 right-8 h-10 w-12 bg-primary rounded-md glassmorphism z-50">
       <button className="h-full w-full flex center">
         <PiDotsThreeBold size={28} />
       </button>
